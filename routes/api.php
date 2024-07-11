be58395349e331ebc0abebb3ece92a1a7ad27b15
kevin huayhuas <kevin.huayhuasgmail.com>
2024-07-11 16:43:27 -0500

    rutas

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,9 @@ use  App\Http\Controllers\Api\V1\LoginController;
 Route::post('v1/login', [loginController::class, 'login']);
 Route::post('v1/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function () {
-    Route::put('users/{user}', [UserController::class, 'update']);
-    Route::delete('users/{user}', [UserController::class, 'destroy']);
-});
+Route::apiResource('v1/user', UserController::class)
+    ->only(['index','show','destroy','update','store']);
+    /*->middleware('auth:sanctum');*/
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::get('users', [UserController::class, 'index']);
-    Route::get('users/{user}', [UserController::class, 'show']);
-    Route::post('users', [UserController::class, 'store']);
-});
 
 
